70820bd93dfb8e87864a1dc53e2a454bdf70679e
sphexxdk <28836823+sphexxdk@users.noreply.github.com>
2018-02-21 19:19:32 +0100

    Add files via upload

---
+++ esx_drugs/config.lua
@@ -0,0 +1,25 @@
+Config              = {}
+Config.MarkerType   = 27
+Config.DrawDistance = 100.0
+Config.ZoneSize     = {x = 3.0, y = 3.0, z = 1.0}
+Config.MarkerColor  = {r = 000, g = 004, b = 000}
+Config.RequiredCopsCoke = 2
+Config.RequiredCopsMeth = 3
+Config.RequiredCopsWeed = 0
+Config.RequiredCopsOpium = 3
+Config.Locale = 'en'
+
+Config.Zones = {
+	CokeFarm = 		 {x=-2342.4880371094,  y=2638.4323730469, z=1.6085621118546},
+	CokeTreatment =  {x=1101.1042480469, y=-3194.7614746094, z=-38.993476867676},
+	CokeResell = 	 {x=-1756.1984863281, y=427.31674194336,  z=126.68292999268},
+	MethFarm = 		 {x=-1146.6766357422, 	 y=4939.7260742188,  z=222.26852416992},
+	MethTreatment =  {x=1003.8566894531,  y=-3199.6340332031,  z=-38.9931640625},
+	MethResell = 	 {x=-63.592178344727, y=-1224.0709228516, z=27.768648147583},
+	WeedFarm = 		 {x=2222.6301269531,  y=5577.3662109375,  z=53.838874816895},
+	WeedTreatment =  {x=1036.5826416016,  y=-3204.9985351563,  z=-38.172500610352},
+	WeedResell = 	 {x=-54.249694824219, y=-1443.3666992188, z=31.068626403809},
+	OpiumFarm = 	 {x=-893.78771972656,	 y=6038.4970703125,  z=42.355514526367},
+	OpiumTreatment = {x=962.67004394531,y=-3003.9489746094, z=-39.639869689941},
+	OpiumResell = 	 {x=-300.76672363281,y=6256.7280273438,z=31.493379592896}
+}
